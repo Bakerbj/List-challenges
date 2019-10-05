329ee8ca6ada3b409fae3eaf6f7f7883f2e2b40c
Brennan Baker <bakerbj26@gmail.com>
2019-10-05 13:31:43 -0600

    Added Ghappy

---
+++ ghappy.rb
@@ -0,0 +1,18 @@
+def ghappy(str)
+    str.size.times do |n|
+        if str[n]= "g" || str[n]= "G"
+            if str[n+1] != "g" && str[n+1] != "G" && str[n-1] != "g" && str[n-1] != "G"
+                return false
+            end
+        end
+    end
+return true
+    
+
+end
+
+
+
+puts ghappy("pasdgnkpaophopgaoj")#false
+puts ghappy("ggso fkjdoaggppa paj dosgg")#true
+puts ghappy("lkjhlhionj")#indeterminable
