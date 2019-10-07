3040ce765e6e9efa3ecc79b3f01f773c09e5f689
Brennan Baker <bakerbj26@gmail.com>
2019-10-07 07:35:47 -0600

    created can_balance

---
+++ can_balance.rb
@@ -0,0 +1,10 @@
+def balanced(list)
+    list.times do |n|
+        list.times do |i|
+            
+        end
+    end
+end
+
+puts balanced([5, 7, 1, 5, 7, 3, 5, 3])
+puts balanced([10, 3, 2, 4, 1])
